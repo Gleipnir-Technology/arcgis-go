bda5a4c7cd4240ddc845eae743e8a07eb6fd87ff
Eli Ribble <eli@gleipnir.technology>
2026-02-12 02:33:51 +0000

    Get username and password, use pretty log

--- examples/map-tile/main.go
+++ examples/map-tile/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	//"fmt"
+	"os"
 
 	//"github.com/Gleipnir-Technology/arcgis-go"
 	"github.com/rs/zerolog"
@@ -10,5 +11,17 @@ import (
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	log.Print("Hello world")
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	var password = os.Getenv("PASSWORD")
+	var username = os.Getenv("USERNAME")
+
+	if password == "" {
+		log.Error().Msg("Cannot have empty password")
+		os.Exit(1)
+	}
+	if username == "" {
+		log.Error().Msg("Cannot have empty username")
+		os.Exit(1)
+	}
+	log.Info().Str("username", username).Str("password", password).Msg("creds")
 }
