5c5814f8eaeb2afb28ca8478340e146b46001b3c
Eli Ribble <eli@gleipnir.technology>
2026-02-12 20:52:57 +0000

    Allow passing in lat/lng as arguments

--- examples/map-tile/main.go
+++ examples/map-tile/main.go
@@ -5,7 +5,8 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
-	//"fmt"
+	"flag"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -25,6 +26,10 @@ func main() {
 	var password = os.Getenv("PASSWORD")
 	var username = os.Getenv("USERNAME")
 
+	var lat = flag.Float64("lat", 37.332003, "The latitude to pull the tile from")
+	var lng = flag.Float64("lng", -122.0307812, "The longitude to pull the tile from")
+	flag.Parse()
+
 	if base == "" {
 		base = "https://www.arcgis.com"
 	}
@@ -36,6 +41,7 @@ func main() {
 		log.Error().Msg("Cannot have empty username")
 		os.Exit(1)
 	}
+
 	ctx := context.TODO()
 	ctx = log.With().Str("component", "arcgis").Logger().WithContext(ctx)
 
@@ -83,26 +89,39 @@ func main() {
 		os.Exit(3)
 	}
 	for _, s := range map_services {
-		log.Info().Str("id", s.ID).Str("name", s.Name).Str("url", s.URL).Str("title", s.Title).Msg("Map service")
-		img, err := s.TileGPS(ctx, gis, 15, 36.322155, -119.33614)
+		log.Info().Str("id", s.ID).Str("name", s.Name).Str("url", s.URL).Str("title", s.Title).Msg("Extracting tiles from map service")
+		level := 14
+		parent_dir := fmt.Sprintf("tiles/%s", s.Name)
+		err := os.MkdirAll(parent_dir, 0750)
 		if err != nil {
-			log.Error().Err(err).Msg("tile failure")
+			log.Error().Err(err).Str("parent_dir", parent_dir).Msg("Failed to make parent dir")
 			os.Exit(4)
 		}
-		// Create file in configured directory
-		filename := "tile.raw"
-		dst, err := os.Create(filename)
-		if err != nil {
-			log.Error().Err(err).Msg("file create failure")
-			os.Exit(4)
+		for {
+			filename := fmt.Sprintf("%s/%d.jpg", parent_dir, level)
+			img, err := s.TileGPS(ctx, gis, level, *lat, *lng)
+			if err != nil {
+				log.Error().Err(err).Msg("tile failure")
+				os.Exit(4)
+			}
+			// Create file in configured directory
+			dst, err := os.Create(filename)
+			if err != nil {
+				log.Error().Err(err).Msg("file create failure")
+				os.Exit(4)
+			}
+			defer dst.Close()
+			// Copy rest of request body to file
+			_, err = io.Copy(dst, bytes.NewReader(img))
+			if err != nil {
+				log.Error().Err(err).Msg("file copy failure")
+				os.Exit(4)
+			}
+			log.Info().Str("filename", filename).Msg("Wrote file")
+			level += 1
+			if level > 30 {
+				break
+			}
 		}
-		defer dst.Close()
-		// Copy rest of request body to file
-		_, err = io.Copy(dst, bytes.NewReader(img))
-		if err != nil {
-			log.Error().Err(err).Msg("file copy failure")
-			os.Exit(4)
-		}
-		log.Info().Str("filename", filename).Msg("Wrote file")
 	}
 }
